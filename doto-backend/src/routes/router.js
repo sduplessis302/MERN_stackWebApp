594f887285d3754e7376cfa1c0223f92ff283d1e
shane-dp <sdup751@aucklanduni.ac.nz>
2020-03-13 16:41:07 +1300

    issue/56-set-up-routes-for-backend
    routes to get/add/delete tasks

--- doto-backend/src/routes/router.js
+++ doto-backend/src/routes/router.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const Task = require('../models/Task');
 
+/*
 // GET ALL users
+
 router.get('/users', function(req, res){
     let users = User.find({}, function(err, users){
         if(err){
@@ -15,22 +18,76 @@ router.get('/users', function(req, res){
         }
     })
 })
+*/
 
 // ADD user
 router.post('/users/add', function (req, res) {
 
     let user = new User();
     user.name = req.body.name;
-    user.email = req.body.email;
-    user.password = req.body.password;
+    user.picture = req.body.picture;
+    user.themePreference = req.body.themePreference;
 
     user.save(function(err){
         if(err){
             console.log(err);
             res.json({msg: "failed..."})
         }
-        else{
+        else{   
             res.json(user)
+        }
+    });
+});
+
+
+//GET ALL task
+router.get('/schedule/get/:username', function(req, res){
+    let tasks = Task.find({}, function(req, res){
+        if(err){
+            console.log(err);
+            res.json({msg: "error retriving tasks..."});
+        } else {
+            console.log(tasks);
+            res.json(tasks);
+        }
+    })
+
+});
+
+//ADD task 
+router.post('/schedule/post', function(req, res){
+
+    let task = new Task();
+    task.user = req.body.user;
+    task.taskID = req.body.taskID;
+    task.title = req.body.title;
+    task.description = req.body.description;
+    task.location = req.body.location;
+    task.priority = req.body.priority;
+    task.duration = req.body.duration;
+    task.startDate = req.body.startDate;
+    task.endDate = req.body.endDate;
+    task.reminder = req.body.reminderDate;
+
+    task.save(function(err){
+        if(err){
+            console.log(err);
+            res.json({msg: "error adding task..."});
+        } else {
+            res.json(task)
+        }
+    });
+});
+
+//DELETE task
+router.post('/api/schedule/:taskid', function(req, res){
+    
+    Task.remove(function(err){
+        if(err){
+            console.log(err);
+            res.json({msg: "error deleting task..."});
+        } else {
+            res.json({msg: "delete successful"});
         }
     });
 });
